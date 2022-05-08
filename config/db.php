6bc4fa63c6d2708b463c64bd55a2c7b300c62282
zouyi <zougangmu@qq.com>
2022-05-08 11:45:43 +0800

    init files by gii

--- config/db.php
+++ config/db.php
@@ -2,13 +2,13 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=supplier',
     'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+    'password' => 'root',
+    'charset' => 'utf8mb4',
 
     // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+//    'enableSchemaCache' => true,
+//    'schemaCacheDuration' => 60,
+//    'schemaCache' => 'cache',
 ];
